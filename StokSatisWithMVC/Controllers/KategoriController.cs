7f0e378c3c62564b2f6ad50f90a2b64f8f0cdfa0
AdnanFurkanAKTEMUR <60827468+AdnanFurkanAKTEMUR@users.noreply.github.com>
2021-04-17 23:02:44 +0300

    _MainLayout ve Kategori listeleme sayfası oluşturuldu

---
+++ StokSatisWithMVC/Controllers/KategoriController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StokSatisWithMVC.Models.Entity;
+
+namespace StokSatisWithMVC.Controllers
+{
+    public class KategoriController : Controller
+    {
+        Dbo_MvcStokEntities db = new Dbo_MvcStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLKATEGORI.ToList();
+            return View(degerler);
+        }
+    }
+}
