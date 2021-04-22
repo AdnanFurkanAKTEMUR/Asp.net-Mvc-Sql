3dd25de47d4c921fa083e797dd3be2f0d3ca07b9
AdnanFurkanAKTEMUR <60827468+AdnanFurkanAKTEMUR@users.noreply.github.com>
2021-04-22 03:31:09 +0300

    Ürün ayrıntıları, Yorumlar ve Yorum yapma eklendi

--- TeknolojiMarketimWithMvc/Controllers/ButunUrunlerController.cs
+++ TeknolojiMarketimWithMvc/Controllers/ButunUrunlerController.cs
@@ -14,14 +14,38 @@ namespace TeknolojiMarketimWithMvc.Controllers
         Dbo_EticaretEntities7 db = new Dbo_EticaretEntities7();
         public ActionResult Index()
         {
-            
+
             var urunler = db.TBLURUNLER.ToList();
             return View(urunler);
         }
 
-        public ActionResult UrunAyrintilari(int urunId=1)
+        [HttpGet]
+        public ActionResult URUNAYRINTI(int id)
         {
-            
+            var urun = db.TBLURUNLER.Where(m => m.URUNID == id).ToList();
+            var yorum = db.TBLYORUM.Where(m => m.YORUNURUN == id).ToList();
+            var urunViewModel = new UrunViewModel
+            {
+                urun = urun,
+                yorumlar = yorum
+            };
+
+
+            return View("URUNAYRINTI", urunViewModel);
+        }
+        
+        [HttpPost]
+        public ActionResult URUNAYRINTI(TBLYORUM p1)
+        {
+            db.TBLYORUM.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        /*
+        public ActionResult UrunAyrintilari(int urunId)
+        {
+
             var urun = db.TBLURUNLER.Where(m => m.URUNID == urunId).ToList();
             var yorum = db.TBLYORUM.Where(m => m.YORUNURUN == urunId).ToList();
             var urunViewModel = new UrunViewModel
@@ -29,9 +53,26 @@ namespace TeknolojiMarketimWithMvc.Controllers
                 urun = urun,
                 yorumlar = yorum
             };
-            
 
-            return View(urunViewModel);
+
+            return View ("UrunAyrintilari",urunViewModel);
         }
+
+
+        
+        /*public ActionResult UrunAyrintilari(TBLYORUM p1)
+        {
+            db.TBLYORUM.Add(p1);
+            db.SaveChanges();
+            /*var urun = db.TBLURUNLER.Where(m => m.URUNID == p1.YORUNURUN).ToList();
+            var yorum = db.TBLYORUM.Where(m => m.YORUNURUN == p1.YORUNURUN).ToList();
+            var urunViewModel = new UrunViewModel
+            {
+                urun = urun,
+                yorumlar = yorum
+            };
+            return View(urunViewModel);
+            return RedirectToAction("Index");
+        }*/
     }
 }
