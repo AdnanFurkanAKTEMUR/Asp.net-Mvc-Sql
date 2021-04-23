7376873f617c1ba08be7bb52721d3a4b738f3b53
AdnanFurkanAKTEMUR <60827468+AdnanFurkanAKTEMUR@users.noreply.github.com>
2021-04-24 00:27:28 +0300

    Yeni Güncellemeler ve Veri Tabanı Yedeği Eklendi

--- TeknolojiMarketimWithMvc/App_Start/RouteConfig.cs
+++ TeknolojiMarketimWithMvc/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TeknolojiMarketimWithMvc
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "AnaSayfa", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ TeknolojiMarketimWithMvc/Controllers/UyeOlController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TeknolojiMarketimWithMvc.Models.Entity;
+
+namespace TeknolojiMarketimWithMvc.Controllers
+{
+    public class UyeOlController : Controller
+    {
+        Dbo_EticaretEntities7 db = new Dbo_EticaretEntities7();
+        // GET: UyeOl
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(TBLMUSTERI m1)
+        {
+            db.TBLMUSTERI.Add(m1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
